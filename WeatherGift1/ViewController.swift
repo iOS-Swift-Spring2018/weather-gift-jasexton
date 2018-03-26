6e3dfe1c0c7bca34cf18b733e48beaf9ae9d07fd
Jack Sexton <jack@jacks-mbp-3.bc.edu>
2018-03-26 11:26:49 -0400

    Initial Commit

---
+++ WeatherGift1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherGift1
+//
+//  Created by Jack Sexton on 3/26/18.
+//  Copyright © 2018 Jack Sexton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
